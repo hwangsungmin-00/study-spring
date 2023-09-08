6c315dba50e5a70607f4c85927db88d27f7e4464
hwangsungmin <hwangsungmin_00@naver.com>
2023-09-08 13:42:29 +0900

    VIEW 환경설정

---
+++ src/main/java/minimin/springstudy/controller/HelloController.java
@@ -0,0 +1,14 @@
+package minimin.springstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello"; //template에 있는 html파일 이름과 같으니 거기로 찾아간다. 
+    }
+}
