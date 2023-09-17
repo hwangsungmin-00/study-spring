9ba82f10158f57e78c8319d28ab5b0a34b82ce09
hwangsungmin <hwangsungmin_00@naver.com>
2023-09-17 13:20:29 +0900

    정적 컨텐츠

--- src/main/java/minimin/springstudy/controller/HelloController.java
+++ src/main/java/minimin/springstudy/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @GetMapping
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
-        return "hello"; //template에 있는 html파일 이름과 같으니 거기로 찾아간다. 
+        return "hello"; //template에 있는 html파일 이름과 같으니 거기로 찾아간다.
     }
 }
